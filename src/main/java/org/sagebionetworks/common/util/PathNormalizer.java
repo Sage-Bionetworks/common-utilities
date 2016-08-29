5d1f06ab34f86e19b7860d117563bc095231638e
Ziming Dong <zimingd@gmail.com>
2016-08-29 13:42:02 -0700

    removed unnecessary parameter. added a test

--- src/main/java/org/sagebionetworks/common/util/PathNormalizer.java
+++ src/main/java/org/sagebionetworks/common/util/PathNormalizer.java
@@ -3,6 +3,9 @@ package org.sagebionetworks.common.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Utility class for normalizing request paths
+ */
 public class PathNormalizer {
 	private static final String AUTH_V1 = "/auth/v1";
 	private static final String FILE_V1 = "/file/v1";
@@ -18,21 +21,23 @@ public class PathNormalizer {
 	 * Normalize the access record's request path
 	 * 
 	 * @param requestPath
-	 * @param method
 	 * @return
 	 */
-	public static String normalizeMethodSignature(String requestPath, String method) {
+	public static String normalizeMethodSignature(String requestPath) {
 		requestPath = requestPath.toLowerCase();
 		if (requestPath.startsWith(REPO_V1) || requestPath.startsWith(FILE_V1) || requestPath.startsWith(AUTH_V1)) {
 			requestPath = requestPath.substring(8);
+		}else{
+			throw new IllegalArgumentException("requestPath must start with " + REPO_V1 + ", " + FILE_V1 + ", or " + AUTH_V1);
 		}
-		if (method.equals("GET") && requestPath.startsWith(GET_MD5_URL_PART)) {
+		
+		if (requestPath.startsWith(GET_MD5_URL_PART)) {
 			return GET_MD5_URL_PART + NUMBER_REPLACEMENT;
 		}
-		if (method.equals("GET") && requestPath.startsWith(GET_EVALUATION_NAME_URL_PART)) {
+		if (requestPath.startsWith(GET_EVALUATION_NAME_URL_PART)) {
 			return GET_EVALUATION_NAME_URL_PART + NUMBER_REPLACEMENT;
 		}
-		if (method.equals("GET") && requestPath.startsWith(GET_ENTITY_ALIAS_URL_PART)) {
+		if (requestPath.startsWith(GET_ENTITY_ALIAS_URL_PART)) {
 			return GET_ENTITY_ALIAS_URL_PART + NUMBER_REPLACEMENT;
 		}
 		Matcher matcher = NUMERIC_PARAM_PATTERN.matcher(requestPath);
