98a957dc2b1963775c48cb32f5a85225a4d4025a
john-hill <john.hill@sagebase.org>
2017-06-30 12:06:39 -0700

    sp

--- src/main/java/org/sagebionetworks/common/util/progress/ProgressListener.java
+++ src/main/java/org/sagebionetworks/common/util/progress/ProgressListener.java
@@ -11,10 +11,10 @@ public interface ProgressListener {
 	 * 
 	 * Note: Progress events will be fired on a separate thread from the thread
 	 * the worker is running on. This allows progress events to be generated
-	 * even when the worker thread is block. Therefore, thread safety must be
+	 * even when the worker thread is blocked. Therefore, thread safety must be
 	 * exercised for all objects access by both the progress listener and
 	 * worker. Also, progress listeners cannot participate in database
-	 * transactions started by the worker, and vis versa.
+	 * transactions started by the worker, and vice versa.
 	 */
 	public void progressMade();
 }
